d2ee8da5735032312054cccc9ffc4d9337b3e66e
nerona <690115491@qq.com>
2016-05-17 11:46:52 +0800

    yaochi
    yaochi

--- sea/dialog.js
+++ sea/dialog.js
@@ -1,3 +1,11 @@
 /**
  * Created by dell on 2016/4/22.
  */
+define(function(require, exports){
+    "use strict";
+    var util = require('./util');
+
+    exports.init = function(){
+      //
+    };
+});
