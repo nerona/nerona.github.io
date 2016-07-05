e5aa2b5f2f297963b5608cf0972b7be0de509aad
nerona <690115491@qq.com>
2016-07-05 11:41:58 +0800

    kefu system

---
+++ kefu/js/common-compiled.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * Created by dell on 2016/7/5.
+ */
+var common = common || {};
+
+common.Util = {};
+
+//# sourceMappingURL=common-compiled.js.map
---
+++ kefu/js/common.js
@@ -0,0 +1,8 @@
+/**
+ * Created by dell on 2016/7/5.
+ */
+var common = common || {};
+
+common.Util = {
+    
+};
