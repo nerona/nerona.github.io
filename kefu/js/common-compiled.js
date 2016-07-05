2729db8a17d1937928429087f1297cd7a3b2454a
nerona <690115491@qq.com>
2016-07-05 17:01:55 +0800

    ...

--- kefu/js/common-compiled.js
+++ kefu/js/common-compiled.js
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 
 /**
  * Created by dell on 2016/7/5.
  */
 
 $(function () {
-    $('.pag-item').on('click', function () {
-        $(this).parent().find('.pag-active').removeClass('pag-active');
-        $(this).addClass('pag-active');
-    });
-    $('.pag-paper').on('click', function () {
-        $(this).parent().find('.pag-active').removeClass('pag-active');
-        $(this).addClass('pag-active');
-    });
+    /*    $('.pag-item').on('click', function(){
+            $(this).parent().find('.pag-active').removeClass('pag-active');
+            $(this).addClass('pag-active');
+        });
+        $('.pag-paper').on('click', function(){
+            $(this).parent().find('.pag-active').removeClass('pag-active');
+            $(this).addClass('pag-active');
+        });*/
 });
 
 var common = common || {};
--- kefu/js/common.js
+++ kefu/js/common.js
@@ -3,14 +3,14 @@
  */
 
 $(function(){
-    $('.pag-item').on('click', function(){
+/*    $('.pag-item').on('click', function(){
         $(this).parent().find('.pag-active').removeClass('pag-active');
         $(this).addClass('pag-active');
     });
     $('.pag-paper').on('click', function(){
         $(this).parent().find('.pag-active').removeClass('pag-active');
         $(this).addClass('pag-active');
-    });
+    });*/
 });
 
 var common = common || {};
