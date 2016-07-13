08ecf9e7cd49c5b033a962858856340fb43f5270
nerona <690115491@qq.com>
2016-07-13 22:16:50 +0800

    ...

--- china_tea/www/js/customer/search/search-special.js
+++ china_tea/www/js/customer/search/search-special.js
@@ -409,9 +409,7 @@ customer.search = {
             console.log(end_year + "-" + end_month + "-" + end_date + "-" +end_hour + "-" + end_min + "-"+end_sec);
             console.log(now_year + "-" + now_month + "-" + now_date + "-" +now_hour + "-" + now_min + "-"+now_sec);
             console.log("--------------------------------------------------------");
-            data[i].hour = end_hour;
-            data[i].min = end_min;
-            data[i].sec = end_sec;
+           
             //已结束
             if(end_year < now_year){
                 data[i].timeTitle = '已结束!';
