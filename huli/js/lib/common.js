e5b052c74c0ab3f0773e46502fcf593cd61c13ee
nerona <690115491@qq.com>
2016-09-25 19:30:23 +0800

    ./.

--- huli/js/lib/common.js
+++ huli/js/lib/common.js
@@ -1,12 +1,3 @@
-(function () {
-    document.addEventListener('DOMContentLoaded', function () {
-        var html = document.documentElement;
-        var windowWidth = html.clientWidth;
-        html.style.fontSize = windowWidth / 7.5 + 'px';
-    }, false);
-})();
-
-
 var common = common || {};
 
 common.Util = {
