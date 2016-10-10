8ade7996921a891b9ea1a6545ad32881df5f234b
nerona <690115491@qq.com>
2016-10-10 09:21:44 +0800

    1234

---
+++ huli/js/create/youhui.js
@@ -0,0 +1,73 @@
+var myScroll;// 引用滑动分页需定义此固定变量myScroll，下拉时刷新使用
+/**
+ * 下拉刷新 （自定义实现此方法）
+ * 此处用延迟模拟数据，
+ */
+function pullDownAction() {
+    /**
+     *此处填写加载后台数据代码
+     * 结束处记得要调用刷新myScroll.refresh();
+     **/
+    myScroll.refresh();
+}
+/**
+ * 滚动翻页 （自定义实现此方法）
+ */
+function pullUpAction() {
+    customer.create.loadMore();
+}
+var Loader = {
+    isLoading: false,
+    request: function (url, data, cb) {
+        var loader = this;
+        if (loader.isLoading) {
+            return;
+        }
+        loader.isLoading = true;
+        common.Util.executeGetAjaxCallback(url, data, function () {
+            loader.isLoading = false;
+            cb.apply(null, arguments);
+        });
+    }
+};
+
+var customer = customer || {};
+
+var url = common.Util.baseUrl + "favouredPolicy/listByPage.do";
+var param = {
+    "page": 1,
+    "limit": 8,
+    "start": 0
+};
+
+customer.create = {
+    init: function() {
+        common.Util.executeGetAjaxCallback(url, param, function (data) {
+            if(data.success == true) {
+                var result = data.entities.favouredPolicies;
+                console.log(result);
+                common.Util.loadTemplate('#list', '#list-t', result);
+            }
+        })
+    },
+    loadMore: function () {
+        param.page++;
+        param.start += 8;
+
+        Loader.request(url, param, function (data) {
+            if (data.success == true) {
+                var result = data.entities.favouredPolicies;
+                if(result != '') {
+                    var tpl = $("#list-t").tmpl(result);
+                    $("#list").append(tpl);
+                } else {
+                    $('.no-more').show();
+                    setTimeout(function(){
+                        $('.no-more').hide();
+                    }, 1000);
+                }
+            }
+            myScroll.refresh();		//调用刷新页面myScroll.refresh();
+        });
+    }
+};
--- huli/js/lib/common.js
+++ huli/js/lib/common.js
@@ -12,7 +12,20 @@
 
 var common = common || {};
 
+
 common.Util = {
+    //配置全局根路径http://120.26.241.5:80/hesheng/
+    baseUrl: 'http://www.xmemoo.cn/hulijiedao/',
+    //配置全局版本号
+    versionCode: 'v1.0',
+    //获取html页面直接跳转参数值
+    getParameter: function (name) {
+        var re = new RegExp("[\?|\&]" + name + "=([^\&]*)", "i");
+        var a = re.exec(document.location.search);
+        if (a == null)
+            return null;
+        return decodeURIComponent(a[1]);
+    },
     setWxTitle:function(title){
         setTimeout(function(){
             $('head title').html(title);
@@ -28,6 +41,42 @@ common.Util = {
             }, 0);
         }).appendTo($body);
     },
+    //ajax回调获取post方式
+    executeAjaxCallback: function (url, param, callbackFun) {
+        $.post(url, param, function (result) {
+            console.log("success\nurl:" + url + "\nparam:" + JSON.stringify(param) + "\nresult:" + JSON.stringify(result));
+            callbackFun(result);
+        }, 'json');
+    },
+    //ajax回调获取get方式
+    executeGetAjaxCallback: function (url, param, callbackFun) {
+        $.get(url, param, function (result) {
+            console.log("success\nurl:" + url + "\nparam:" + JSON.stringify(param) + "\nresult:" + JSON.stringify(result));
+            callbackFun(result);
+        }, 'json');
+    },
+    //ajax回调获取get方式，不建议使用，统一使用post
+    executeGetJsonAjaxCallback: function (url, param, callbackFun) {
+        $.getJSON(url, param, function (result) {
+            console.log("success\nurl:" + url + "\nparam:" + JSON.stringify(param) + "\nresult:" + JSON.stringify(result));
+            callbackFun(result);
+        });
+    },
+    //jsonp回调获取get方式
+    executeGetJsonpCallback: function (url, param, callbackFun) {
+        $.ajax({
+            url: url,
+            data: param,
+            type: 'get',
+            dataType: 'jsonp',
+            success: function(data){
+                callbackFun(data);
+            }
+        });
+    },
+    loadTemplate:function(render ,templateId ,data ){
+        $(render).html($(templateId).tmpl(data));
+    }
 };
 /*定义校验工具对象*/
 common.validate = {
@@ -75,22 +124,5 @@ common.toast = {
 };
 
 $(function () {
-
     console.info(document.location.href);
-
-    //wx
-    if (common.validate.isWeiXin()) {
-        //$('.bar').hide();
-        //$('.bar-nav~.content').css('top', '0');
-    }
-    //android
-    if (common.validate.isAndroid()) {
-        //$('.bar').hide();
-        //$('.bar-nav~.content').css('top', '0');
-    }
-    //ios
-    if (common.validate.isIos()) {
-        //$('.bar').hide();
-        //$('.bar-nav~.content').css('top', '0');
-    }
 });
